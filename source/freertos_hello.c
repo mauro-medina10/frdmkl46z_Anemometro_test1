46eb5040ef625a47d8d8723fc25d5b2bb398e082
mauro-medina10 <mauro93medina@gmail.com>
2019-09-14 11:56:24 -0300

    Cambia el comportamiento, con evento sw se habilita o desactiva el blinkeo

--- source/freertos_hello.c
+++ source/freertos_hello.c
@@ -46,6 +46,7 @@
 #include "board_dsi.h"
 #include "stdio.h"
 #include "pin_mux.h"
+#include "key.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -53,6 +54,7 @@
 typedef struct
 {
     board_ledId_enum idLed;
+    board_swId_enum idSW;
     uint32_t semiPeriodo;
 }paramBlinked_t;
 
@@ -63,8 +65,9 @@ typedef struct
  ******************************************************************************/
 static void blinky_task(void *pvParameters);
 
-paramBlinked_t redLed = {BOARD_LED_ID_ROJO , 500};
-paramBlinked_t greenLed = {BOARD_LED_ID_VERDE , 750};
+static const paramBlinked_t redLed = {BOARD_LED_ID_ROJO ,BOARD_SW_ID_1, 500};
+static const paramBlinked_t greenLed = {BOARD_LED_ID_VERDE,BOARD_SW_ID_3 , 750};
+
 
 /*******************************************************************************
  * Code
@@ -77,37 +80,55 @@ int main(void)
     /* Init board hardware. */
     board_init();
 
-    if (xTaskCreate(blinky_task, "red_blinky_task", 100, &redLed, blinky_task_PRIORITY, NULL) != pdPASS)
+    key_init();
+
+    if (xTaskCreate(blinky_task, "red_blinky_task", configMINIMAL_STACK_SIZE,(void * const) &redLed, blinky_task_PRIORITY, NULL) != pdPASS)
     {
         printf("Error creacion task 1");
         while (1)
             ;
     }
-    if (xTaskCreate(blinky_task, "green_blinky_task", 100, &greenLed, blinky_task_PRIORITY, NULL) != pdPASS)
+    if (xTaskCreate(blinky_task, "green_blinky_task", configMINIMAL_STACK_SIZE, (void * const)&greenLed, blinky_task_PRIORITY, NULL) != pdPASS)
     {
         printf("Error creacion task 2");
         while (1)
             ;
     }
+
     vTaskStartScheduler();
     for (;;)
         ;
 }
-
-/*!
- * @brief Task responsible for printing of "Hello world." message.
- */
+/* Cuando se pulsa un sw empieza a parpadear el led o se apaga*/
 static void blinky_task(void *pvParameters)
 {
     paramBlinked_t *paramBlinked;
+    bool swEventflag = 0;
 
     paramBlinked = (paramBlinked_t*) pvParameters;
 
     for (;;)
     {
-        board_setLed(paramBlinked->idLed, BOARD_LED_MSG_TOGGLE);
-        vTaskDelay((paramBlinked->semiPeriodo) / portTICK_PERIOD_MS); /* Esta configurado en 1 tick cada 5ms (el minimo es 1ms) */
+        if(key_getPressEv(paramBlinked->idSW))
+            {
+            swEventflag = !swEventflag;
+            }
+        if(swEventflag)
+        {
+            board_setLed(paramBlinked->idLed, BOARD_LED_MSG_TOGGLE);
+            vTaskDelay((paramBlinked->semiPeriodo) / portTICK_PERIOD_MS); /* Esta configurado en 1 tick cada 5ms (el minimo es 1ms) */
+        }
+        else
+        {
+            board_setLed(paramBlinked->idLed, BOARD_LED_MSG_OFF);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
+}
+
+void vApplicationTickHook(void)
+{
+    key_periodicTask1ms();
 }
 
 extern void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName )
